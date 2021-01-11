022f20856081cc8f329b2db21981aea14ff32634
Sergio García Álvarez <alvarez.garcia.sergio@gmail.com>
2021-01-11 17:27:18 +0100

    version final con javadoc DNI 29203578

--- src/EmpleadoED_29203578H.java
+++ src/EmpleadoED_29203578H.java
@@ -2,17 +2,39 @@ package empleadoED_29203578h;
 
 import java.util.*;
 
+/**
+ * Ejercicio práctico examen EVAL 1 ED
+ *
+ * @author SERGIO GARCÍA ÁLVAREZ
+ * @version 1.0
+ */
 public class EmpleadoED_29203578H {
 
     static final double FACTOR_HORA_EXTRA = 1.2;
     private String nombreCompleto, dni;
     private double salarioBasePorHora = 10.56;
 
+    /**
+     * Constructor con tres parámetros
+     *
+     * @param nombreCompleto nombre completo en mayúsculas
+     * @param dni documento nacional de identidad con letra y sin espacios
+     */
     public EmpleadoED_29203578H(String nombreCompleto, String dni) {
         this.nombreCompleto = nombreCompleto;
         this.dni = dni;
     }
 
+    /**
+     * Calcula el salario bruto mensual en función del salario base por hora y
+     * las horas extras El preio por hora de la hora extra se determina con la
+     * constante FACTOR_HORA_EXTRA
+     *
+     * @param horasExtra número de horas extra dedicadas redondeado a la baja
+     * (entero)
+     *
+     * @return devuelve el salario bruto mensual en euros con dos decimales
+     */
     public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
         cantidadExtra = horasExtra * this.salarioBasePorHora
@@ -28,5 +50,5 @@ public class EmpleadoED_29203578H {
         EmpleadoED_29203578H emp = new EmpleadoED_29203578H("SERGIO GARCÍA ÁLVAREZ", "29203578H");
         int horasExtra = 2;
         System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra) + " euros, al hacer " + horasExtra + " horas extra.");
- }
+    }
 }
